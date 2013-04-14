0877ce03b9cd1a029c6a05669beeb35652aacd9a
serily <serilyzheng@gmail.com>
2013-04-14 16:56:11 +0800

    commit

--- rule2nfa/rule2nfa.cpp
+++ rule2nfa/rule2nfa.cpp
@@ -2,6 +2,53 @@
 #include "rule2nfa.h"
 #include "CombineTree.h"
 
+struct OPTIONCONTENT : public RULEOPTION
+{
+	std::vector<BYTE> vecconts;
+	int nOffset;
+	int nDepth;
+	int nDistance;
+	int nWithin;
+};
+
+struct OPTIONPCRE : public RULEOPTION
+{
+	std::string strPattern;
+};
+
+enum OPTIONCONTENTFLAGS
+{
+	CF_NOCASE		= (1 << 0),//1
+	CF_OFFSET		= (1 << 1),//2
+	CF_DEPTH		= (1 << 2),//4
+	CF_DISTANCE		= (1 << 3),//8
+	CF_WITHIN		= (1 << 4),//16
+};
+
+enum PCREFLAGS
+{
+	PF_i = (1 << 0),
+	PF_s = (1 << 1),
+	PF_m = (1 << 2),
+	PF_x = (1 << 3),
+	PF_A = (1 << 4),
+	PF_E = (1 << 5),
+	PF_G = (1 << 6),
+	PF_R = (1 << 7),
+	PF_U = (1 << 8),
+	PF_B = (1 << 9),
+	PF_P = (1 << 10),
+	PF_H = (1 << 11),
+	PF_M = (1 << 12),
+	PF_C = (1 << 13),
+	PF_O = (1 << 14),
+	PF_I = (1 << 15),
+	PF_D = (1 << 16),
+	PF_K = (1 << 17),
+	PF_S = (1 << 18),
+	PF_Y = (1 << 19)
+};
+
 struct RULEOPTIONRAW
 {
 	std::string name;
