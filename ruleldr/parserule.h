be94de6864de51b96fbf180f4ce55469267190aa
haoweichen <haowch@gmail.com>
2013-04-14 11:58:20 +0800

    test

---
+++ ruleldr/parserule.h
@@ -0,0 +1,13 @@
+#include "../SnortCommon/snortcommon.h"
+
+#ifndef PARSERULE_H_
+#define PARSERULE __declspec(dllimport)
+#else
+#define PARSERULE __declspec(dllexport)
+#endif
+
+
+typedef void (CALLBACK *RECIEVER)(const CSnortRule &rule);
+
+PARSERULE size_t ParseRule(LPCTSTR fileName, RECIEVER recv);
+
