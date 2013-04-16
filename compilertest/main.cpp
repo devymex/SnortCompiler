638a728b46cfdca3d5568f2274ab83e14499778a
haoweichen <haowch@gmail.com>
2013-04-16 22:59:45 +0800

    tired

--- compilertest/main.cpp
+++ compilertest/main.cpp
@@ -11,42 +11,42 @@ void main()
 	compile(_T("..\\..\\input\\allrules.rule"), result);
 	result.WriteToFile(_T("..\\..\\output\\result.cdt") );
 
-	CRes readRes;
-	readRes.ReadFromFile(_T("..\\..\\output\\result.cdt"));
+	//CRes readRes;
+	//readRes.ReadFromFile(_T("..\\..\\output\\result.cdt"));
 
-	std::ofstream foutBeforeWrite("..\\..\\output\\BeforeWrite.txt");
+	//std::ofstream foutBeforeWrite("..\\..\\output\\BeforeWrite.txt");
 
-	for (size_t i = 0; i < result.GetDfaTable().Size(); ++i)
-	{
-		for (size_t j = 0; j < result.GetDfaTable()[i].Size(); ++j)
-		{
-			foutBeforeWrite << j << ":";
-			for (size_t k = 0; k < CHARSETSIZE; ++k)
-			{
-				foutBeforeWrite << "(" << k << "," << result.GetDfaTable()[i][j][k] << ")";
-			}
-			foutBeforeWrite << std::endl;
-		}
-	}
-	foutBeforeWrite.close();
-	foutBeforeWrite.clear();
+	//for (size_t i = 0; i < result.GetDfaTable().Size(); ++i)
+	//{
+	//	for (size_t j = 0; j < result.GetDfaTable()[i].Size(); ++j)
+	//	{
+	//		foutBeforeWrite << j << ":";
+	//		for (size_t k = 0; k < CHARSETSIZE; ++k)
+	//		{
+	//			foutBeforeWrite << "(" << k << "," << result.GetDfaTable()[i][j][k] << ")";
+	//		}
+	//		foutBeforeWrite << std::endl;
+	//	}
+	//}
+	//foutBeforeWrite.close();
+	//foutBeforeWrite.clear();
 
-	std::ofstream foutAfterRead("..\\..\\output\\AfterRead.txt");
+	//std::ofstream foutAfterRead("..\\..\\output\\AfterRead.txt");
 
-	for (size_t i = 0; i < result.GetDfaTable().Size(); ++i)
-	{
-		for (size_t j = 0; j < result.GetDfaTable()[i].Size(); ++j)
-		{
-			foutAfterRead << j << ":";
-			for (size_t k = 0; k < CHARSETSIZE; ++k)
-			{
-				foutAfterRead << "(" << k << "," << result.GetDfaTable()[i][j][k] << ")";
-			}
-			foutAfterRead << std::endl;
-		}
-	}
-	foutAfterRead.close();
-	foutAfterRead.clear();
+	//for (size_t i = 0; i < result.GetDfaTable().Size(); ++i)
+	//{
+	//	for (size_t j = 0; j < result.GetDfaTable()[i].Size(); ++j)
+	//	{
+	//		foutAfterRead << j << ":";
+	//		for (size_t k = 0; k < CHARSETSIZE; ++k)
+	//		{
+	//			foutAfterRead << "(" << k << "," << result.GetDfaTable()[i][j][k] << ")";
+	//		}
+	//		foutAfterRead << std::endl;
+	//	}
+	//}
+	//foutAfterRead.close();
+	//foutAfterRead.clear();
 
 	system("pause");
 }
