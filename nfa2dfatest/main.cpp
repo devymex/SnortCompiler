0e66ee704241e4546278dbc08ee48056decc4b25
houqigulugulu <houqigulugulu@gmail.com>
2013-04-22 17:59:04 +0800

    nfa2dfatest

--- nfa2dfatest/main.cpp
+++ nfa2dfatest/main.cpp
@@ -3,6 +3,73 @@
 #include "../pcre2nfa/pcre2nfa.h"
 #include "../nfa2dfa/nfa2dfa.h"
 
+
+template <class _t>
+void printDfa(_t dfaTab)
+{
+	int tabSize = dfaTab.Size();
+	std::vector<std::vector<std::vector<size_t>>> matrix;
+	std::vector<std::vector<size_t>> matRow;
+	std::vector<size_t> matRowRow;
+	for(int i = 0; i < tabSize; ++i)
+	{
+		matrix.push_back(matRow);
+		for(int j = 0; j < tabSize; ++j)
+		{
+			matrix[i].push_back(matRowRow);
+		}
+	}
+
+		for(size_t s = 0; s < dfaTab.Size(); ++s)
+	{
+		for(int i = 0 ; i < CHARSETSIZE - 4; ++i)
+		{
+			if(dfaTab[s][i] != size_t(-1))
+			{
+				size_t suf = dfaTab[s][i];
+				matrix[s][suf].push_back(i);
+			}
+		}
+
+	}
+
+
+	for(std::vector<std::vector<std::vector<size_t>>>::iterator iter = matrix.begin(); iter != matrix.end(); ++iter)
+	{
+		std::cout << iter - matrix.begin() << "  :  ";
+		for(std::vector<std::vector<size_t>>::iterator curIter = iter->begin(); curIter != iter->end(); ++curIter)
+		{
+			if(curIter->size() > 0)
+			{
+				std::cout << curIter - iter->begin() << " (";
+				for(std::vector<size_t>::iterator curcurIter = curIter->begin(); curcurIter != curIter->end(); ++curcurIter)
+				{
+					std::cout << *curcurIter << ",";
+				}
+				std::cout << ")  ";
+			}
+		}
+		std::cout << std::endl;
+
+	}
+
+}
+
+void printNfa(CNfa &nfa)
+{
+	for (size_t j = 0; j < nfa.Size(); ++j)
+		{
+			std::cout << j << ": ";
+			for (size_t k = 0; k < CHARSETSIZE; ++k)
+			{
+				for (size_t l = 0; l < nfa[j][k].Size(); ++l)
+				{
+					std::cout << "(" << k << "," << nfa[j][k][l] << ")";
+				}
+			}
+			std::cout << std::endl;
+		}
+}
 void main()
 {
 	//const char* a = "/abcd.*ABCD{1024}/";
@@ -12,7 +79,7 @@ void main()
 	//size_t nfasize = nfa.Size();
 	//NfaToDfa(nfa, dfa);
 	//size_t dfasize = dfa.Size();
-	CDfa dfa;
+	/*CDfa dfa;
 	dfa.Resize(5);
 	dfa[0]['a'] = 1;
 	dfa[0]['b'] = 2;
@@ -36,5 +103,26 @@ void main()
 	CDfa mindfa;
 	DfaMin(dfa, mindfa);
 
+	system("pause");*/
+
+	CNfa nfa;
+	CDfa dfa;
+	size_t size = dfa.Size();
+	nfa.Resize(5);
+	nfa[0][EMPTY].PushBack(1);
+	nfa[0][EMPTY].PushBack(3);
+	nfa[1]['a'].PushBack(1);
+	nfa[1]['b'].PushBack(2);
+	nfa[1][EMPTY].PushBack(5);
+	nfa[2]['b'].PushBack(2);
+	nfa[2][EMPTY].PushBack(5);
+	nfa[3]['a'].PushBack(4);
+	nfa[3]['b'].PushBack(4);
+	nfa[4]['a'].PushBack(4);
+	nfa[4][EMPTY].PushBack(5);
+
+	printNfa(nfa);
+	NfaToDfa(nfa,dfa);
+	printDfa(dfa);
 	system("pause");
 }
