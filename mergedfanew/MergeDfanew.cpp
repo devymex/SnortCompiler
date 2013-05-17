4726df9e20924938644a81eaa14c62b6776e66b8
serily <serilyzheng@gmail.com>
2013-05-17 12:00:57 +0800

    modify nfa group

--- mergedfanew/MergeDfanew.cpp
+++ mergedfanew/MergeDfanew.cpp
@@ -3,47 +3,47 @@
 #include "MergeDfanew.h"
 
 //原始代码，有小问题，慢
-//MERDFANEW bool NOrMerge(std::vector<CDfanew> &dfas, CDfanew &lastDfa)
-//{
-//#undef max
-//
-//	size_t nTermSta = 0;//合并后nfa总状态数
-//	for(size_t i = 0; i < dfas.size(); ++i)
-//	{
-//		if(dfas[i].Size() > std::numeric_limits<STATEID>::max())
-//		{
-//			return false;
-//		}
-//		nTermSta += dfas[i].Size();
-//	}
-//	++nTermSta;
-//
-//	NFALOG nfalog[DFACOLSIZE];//nfa状态与dfaID的对应， 这样的数组有问题！
-//	size_t count = 0;
-//
-//	std::vector<CNfa> nfas;
-//	
-//	//STATEID nTermSta = 0;//合并后总状态数
-//	CNfa oneNfa;//合并后的nfa
-//	oneNfa.Reserve(nTermSta + 1);
-//	oneNfa.Resize(1);
-//
-//	for(size_t i = 0; i < dfas.size(); ++i)
-//	{
-//		oneNfa[0].AddDest(EMPTYEDGE, oneNfa.Size() + dfas[i].GetStartId());
-//		NIncreDfaNum(dfas[i], (size_t)oneNfa.Size());
-//		NInsertDfa(dfas[i], oneNfa, nTermSta, nfalog, count);
-//	}
-//
-//	if(lastDfa.FromNFA(oneNfa, nfalog, count, true) == 0)
-//	{
-//		return true;
-//	}
-//	else
-//	{
-//		return false;
-//	}
-//}
+MERDFANEW bool NOrMerge(std::vector<CDfanew> &dfas, CDfanew &lastDfa)
+{
+#undef max
+
+	size_t nTermSta = 0;//合并后nfa总状态数
+	for(size_t i = 0; i < dfas.size(); ++i)
+	{
+		if(dfas[i].Size() > std::numeric_limits<STATEID>::max())
+		{
+			return false;
+		}
+		nTermSta += dfas[i].Size();
+	}
+	++nTermSta;
+
+	NFALOG nfalog[DFACOLSIZE];//nfa状态与dfaID的对应， 这样的数组有问题！
+	size_t count = 0;
+
+	std::vector<CNfa> nfas;
+	
+	//STATEID nTermSta = 0;//合并后总状态数
+	CNfa oneNfa;//合并后的nfa
+	oneNfa.Reserve(nTermSta + 1);
+	oneNfa.Resize(1);
+
+	for(size_t i = 0; i < dfas.size(); ++i)
+	{
+		oneNfa[0].AddDest(EMPTYEDGE, oneNfa.Size() + dfas[i].GetStartId());
+		NIncreDfaNum(dfas[i], (size_t)oneNfa.Size());
+		NInsertDfa(dfas[i], oneNfa, nTermSta, nfalog, count);
+	}
+
+	if(lastDfa.FromNFA(oneNfa, nfalog, count, true) == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 
 struct TODFA_HASH
 {
@@ -65,47 +65,47 @@ struct TODFA_HASH
 
 //直接将多个dfa合并成转换成一个dfa(用nfa表示)，在对nfa分组（也就是对dfa分组）同时填dfa的表
 //因为dfa只有唯一的下一状态跳转，且没有空跳转，因此不用求空闭包
-MERDFANEW bool NOrMerge(std::vector<CDfanew> &dfas, CDfanew &lastDfa)
-{
-#undef max
-
-	size_t nTermSta = 0;//合并后nfa总状态数
-	for(size_t i = 0; i < dfas.size(); ++i)
-	{
-		if(dfas[i].Size() > std::numeric_limits<STATEID>::max())
-		{
-			return false;
-		}
-		nTermSta += dfas[i].Size();
-	}
-	++nTermSta;
-
-	NFALOG nfalog[DFACOLSIZE];//nfa状态与dfaID的对应
-	size_t count = 0;
-
-	typedef std::unordered_map<std::vector<size_t>, STATEID, TODFA_HASH> STATESETHASH;
-	STATESETHASH statehash;
-
-	std::vector<>;
-	
-	CNfa oneNfa;//合并后的nfa
-	oneNfa.Reserve(nTermSta + 1);
-	oneNfa.Resize(1);
-
-	for(size_t i = 0; i < dfas.size(); ++i)
-	{
-		oneNfa[0].AddDest(EMPTYEDGE, oneNfa.Size() + dfas[i].GetStartId());
-		NIncreDfaNum(dfas[i], (size_t)oneNfa.Size());
-		NInsertDfa(dfas[i], oneNfa, nTermSta, nfalog, count);
-	}
-
-	if(lastDfa.FromNFA(oneNfa, nfalog, count, true) == 0)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
+//MERDFANEW bool NOrMerge(std::vector<CDfanew> &dfas, CDfanew &lastDfa)
+//{
+//#undef max
+//
+//	size_t nTermSta = 0;//合并后nfa总状态数
+//	for(size_t i = 0; i < dfas.size(); ++i)
+//	{
+//		if(dfas[i].Size() > std::numeric_limits<STATEID>::max())
+//		{
+//			return false;
+//		}
+//		nTermSta += dfas[i].Size();
+//	}
+//	++nTermSta;
+//
+//	NFALOG nfalog[DFACOLSIZE];//nfa状态与dfaID的对应
+//	size_t count = 0;
+//
+//	typedef std::unordered_map<std::vector<size_t>, STATEID, TODFA_HASH> STATESETHASH;
+//	STATESETHASH statehash;
+//
+//	std::stack<std::vector<size_t> > ;
+//	
+//	CNfa oneNfa;//合并后的nfa
+//	oneNfa.Reserve(nTermSta + 1);
+//	oneNfa.Resize(1);
+//
+//	for(size_t i = 0; i < dfas.size(); ++i)
+//	{
+//		oneNfa[0].AddDest(EMPTYEDGE, oneNfa.Size() + dfas[i].GetStartId());
+//		NIncreDfaNum(dfas[i], (size_t)oneNfa.Size());
+//		NInsertDfa(dfas[i], oneNfa, nTermSta, nfalog, count);
+//	}
+//
+//	if(lastDfa.FromNFA(oneNfa, nfalog, count, true) == 0)
+//	{
+//		return true;
+//	}
+//	else
+//	{
+//		return false;
+//	}
+//}
 
