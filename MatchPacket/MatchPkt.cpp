be385095ee7f8f50e91ae3c62d8e46b16bf220f4
houqigulugulu <houqigulugulu@gmail.com>
2013-05-31 15:50:59 +0800

    matchpkt

--- MatchPacket/MatchPkt.cpp
+++ MatchPacket/MatchPkt.cpp
@@ -72,3 +72,58 @@ MATCHPKT bool LoadCapFile(const char* pFile, void* pUser)
 {
 	return MyLoadCapFile(pFile, PktParam, pUser);
 }
+
+void FindSig(size_t sig, std::map<size_t, std::vector<SIGSID>> &hashtable, std::vector<size_t> &matchSids)
+{
+	if (hashtable.count(sig))
+	{
+		std::vector<SIGSID> &onevec = hashtable[sig];
+		if (!onevec.empty())
+		{
+			for(std::vector<SIGSID>::iterator iter = onevec.begin(); iter != onevec.end(); ++iter)
+			{
+				if (sig == iter->sig)
+				{
+					matchSids.resize(matchSids.size() + 1);
+					matchSids.back() = iter->sig;
+				}
+			}
+		}
+	}
+}
+
+void MatchOnedfa(std::vector<u_char> &onepkt, CDfanew &dfa, std::vector<size_t> &matchedSids)
+{
+
+}
+
+MATCHPKT void MatchPkt(std::vector<std::vector<u_char>> &allPkt, std::map<size_t, std::vector<SIGSID>> &hashtable, std::vector<CDfanew> &alldfas, std::vector<MATCHRESULT> &matchresult)
+{
+	for (std::vector<std::vector<u_char>>::iterator allPktIter = allPkt.begin(); allPktIter != allPkt.end(); ++allPktIter)
+	{
+		SIGNATURE onesig = -1;
+		std::vector<size_t> matchDids;
+		std::vector<size_t> matchedSids;
+
+		for (std::vector<u_char>::iterator pktIter = allPktIter->begin(); pktIter + 3 != allPktIter->end(); ++pktIter)
+		{
+			onesig = *(SIGNATURE *)&(*pktIter);
+			FindSig(HashFcn(onesig), hashtable, matchDids);
+		}
+
+		if (!matchDids.empty())
+		{
+			for(std::vector<size_t>::iterator idIter = matchDids.begin(); idIter != matchDids.end(); ++idIter)
+			{
+				MatchOnedfa(*allPktIter, alldfas[*idIter], matchedSids);
+			}
+		}
+
+		if(!matchedSids.empty())
+		{
+			matchresult.resize(matchresult.size() + 1);
+			matchresult.back().pktnum = allPktIter - allPkt.begin();
+			matchresult.back().matchedSids = matchedSids;
+		}
+	}
+}
--- MatchPacket/MatchPkt.h
+++ MatchPacket/MatchPkt.h
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <vector>
 #include "../common/common.h"
+#include "../dfanew/dfanew.h"
+
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Packet.lib")
 #pragma comment(lib, "wpcap.lib")
