4e258f904ca7b98a56078a55e7992dcb51bd8a5b
houqigulugulu <houqigulugulu@gmail.com>
2013-06-04 16:44:37 +0800

    chan

--- PMatchPktTest/main.cpp
+++ PMatchPktTest/main.cpp
@@ -1,5 +1,6 @@
 #include "../MatchPacket/MatchPkt.h"
 #include "../PMatchPkt/PMatchPkt.h"
+#include <fstream>
 
 #include <iostream>
 #include <tchar.h>
@@ -26,16 +27,19 @@ void GetMchRule(std::vector<u_char> &pkt, REGRULESMAP &rulesmap, std::vector<siz
 void main(size_t argc, char* argv[])
 {
 	std::vector<std::vector<u_char>> allPkt;
-	LoadCapFile("F:\\cppProject\\huawei\\PreciseMatch\\PacketMatch\\2_71_u_2.cap00_up.cap", &allPkt);
+	//LoadCapFile("F:\\cppProject\\huawei\\PreciseMatch\\PacketMatch\\2_71_u_2.cap00_up.cap", &allPkt);
 
 	REGRULESMAP rulesmap;
 	MchCompile(_T("../../input/test.rules"), &rulesmap);
 
-	//u_char p[] = {0, 0, 'f', 'g', 0, 'a', 'b', 'C', 'd', 0, 'd', 'e', 235, 0, 42, 'A', 123, 'B', 40, '1', '2', 93, 63, 'a', 'b', 'c', 'd', 'e', 'a'};
-	//std::vector<u_char> tmpvec(p, p + sizeof(p));
-	//allPkt.push_back (tmpvec);
+	u_char p[] = {0, 0, 'f', 'g', 0, 'a', 'b', 'C', 'd', 0, 'd', 'e', 235, 0, 42, 'A', 123, 'B', 40, '1', '2', 93, 63, 'a', 'b', 'c', 'd', 'e', 'a'};
+	std::vector<u_char> tmpvec(p, p + sizeof(p));
+	allPkt.push_back (tmpvec);
+
+	std::ofstream matchresult("C:\\test\\matchresult.txt");
 	for(std::vector<std::vector<u_char>>::iterator allIter = allPkt.begin(); allIter != allPkt.end(); ++allIter)
 	{
+		matchresult << allIter - allPkt.begin();
 		std::vector<size_t> rules;
 		GetMchRule(*allIter, rulesmap, rules);
 		std::vector<size_t> matchedSid;//保存匹配上的规则的sid
@@ -45,9 +49,14 @@ void main(size_t argc, char* argv[])
 			if(flag)
 			{
 				//匹配成功
+
+				matchresult << rulesmap.result[*rIt].m_nSid << "  ";
 				matchedSid.push_back(rulesmap.result[*rIt].m_nSid);
 			}
 		}
+
+		matchresult << "\n";
 	}
+	matchresult.close();
 	std::cout << rulesmap.sigmap.size();
 }
