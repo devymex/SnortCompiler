f1ae08df7579e9670e8954582a3521ad50ab0de9
houqigulugulu <houqigulugulu@gmail.com>
2013-06-05 21:41:04 +0800

    change

--- matchpkttest/main.cpp
+++ matchpkttest/main.cpp
@@ -9,6 +9,6 @@ void main()
 	REGRULESMAP rulesmap;
 	rulesmap.mchresult.open(_T("../../output/matchresult.txt"));
 	MchCompile(_T("../../input/allrules.rule"), &rulesmap);
-	HandleAllFile("E:\\test", &rulesmap);
+	HandleAllFile("E:\\allPkt", &rulesmap);
 	rulesmap.mchresult.close();
 }
