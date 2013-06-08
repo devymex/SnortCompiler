42187c64f1c4cd053e6493f28ca055d0c67dd497
serily <serilyzheng@gmail.com>
2013-06-08 11:38:57 +0800

    add comments on MergeDfaNew.cpp

--- mergedfanew/MergeDfanew.cpp
+++ mergedfanew/MergeDfanew.cpp
@@ -3,7 +3,8 @@
 **
 **  @author      Lab 435, Xidian University
 **
-**  @brief       merge mutiple dfas into one dfa
+**  @brief       merge mutiple dfas into one dfa and
+**				 mark the merged dfa's terminal states
 **
 */
 
