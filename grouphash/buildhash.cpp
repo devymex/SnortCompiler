12ab34c97b0188c0a48e518d75a7c7600c7723a0
haoweichen <haowch@gmail.com>
2013-06-21 14:58:25 +0800

    modify hash function

--- grouphash/buildhash.cpp
+++ grouphash/buildhash.cpp
@@ -34,14 +34,16 @@ struct SIGHASH
 	ulong nBucketCnt;
 	ulong operator()(const SIGNATURE &oneSig)
 	{
-		const ulong _FNV_offset_basis = 2166136261U;
-		const ulong _FNV_prime = 16777619U;
+		//const ulong _FNV_offset_basis = 2166136261U;
+		//const ulong _FNV_prime = 16777619U;
 
-		ulong _Val = _FNV_offset_basis;
-		_Val ^= oneSig;
-		_Val *= _FNV_prime;
+		//ulong _Val = _FNV_offset_basis;
+		//_Val ^= oneSig;
+		//_Val *= _FNV_prime;
 
-		return (_Val % nBucketCnt);
+		//return (_Val % nBucketCnt);
+
+		return (oneSig % nBucketCnt);
 	}
 }hash;
 
