a92f2904e2b9c50c57393a3254b05dd18165814d
haoweichen <haowch@gmail.com>
2013-06-21 21:37:40 +0800

    move hash from .cpp to .h

--- grouphash/buildhash.cpp
+++ grouphash/buildhash.cpp
@@ -29,24 +29,6 @@ Returns:				hash slot
 
 */
 
-struct SIGHASH
-{
-	ulong nBucketCnt;
-	ulong operator()(const SIGNATURE &oneSig)
-	{
-		//const ulong _FNV_offset_basis = 2166136261U;
-		//const ulong _FNV_prime = 16777619U;
-
-		//ulong _Val = _FNV_offset_basis;
-		//_Val ^= oneSig;
-		//_Val *= _FNV_prime;
-
-		//return (_Val % nBucketCnt);
-
-		return (oneSig % nBucketCnt);
-	}
-}hash;
-
 //one point in the adjust path
 struct STATION
 {
--- include/hwprj/buildhash.h
+++ include/hwprj/buildhash.h
@@ -29,6 +29,24 @@ struct HASHNODE
 	ulong m_nDfaId;
 };
 
+struct SIGHASH
+{
+	ulong nBucketCnt;
+	ulong operator()(const SIGNATURE &oneSig)
+	{
+		//const ulong _FNV_offset_basis = 2166136261U;
+		//const ulong _FNV_prime = 16777619U;
+
+		//ulong _Val = _FNV_offset_basis;
+		//_Val ^= oneSig;
+		//_Val *= _FNV_prime;
+
+		//return (_Val % nBucketCnt);
+
+		return (oneSig % nBucketCnt);
+	}
+}hash;
+
 typedef std::map<ulong, std::vector<HASHNODE>> HASHRES;
 
 HASHMAPHDR void HashMapping(CGroupRes &groupRes, HASHRES &HashResMap);
