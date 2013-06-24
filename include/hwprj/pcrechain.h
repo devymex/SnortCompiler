9a0ce409691202ec83cb12407a6dde6147f2a403
bunnywj <bunny8301@gmail.com>
2013-06-24 15:16:39 +0800

    modify pcrechain

--- include/hwprj/pcrechain.h
+++ include/hwprj/pcrechain.h
@@ -12,6 +12,10 @@
 class PCREVEC;
 #endif
 
+/*! @addtogroup groupCompiler
+*  @{
+*/
+
 /*!
 * @brief		实现PCRE链的数据结构及相关算法
 * @remark		封装了std::vector<class CPcreOption>对象以及Signature数组
@@ -72,3 +76,7 @@ protected:
 	/// @brief		私有成员，仅供内部使用
 	CUnsignedArray			m_sigs;
 };
+
+/*!
+@}
+*/
