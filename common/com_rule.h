e18b980dd73e7b9f556f240ceeddbc440e5bbf9c
devymex <devymex@gmail.com>
2013-06-25 09:59:34 +0800

    add some comments

--- common/com_rule.h
+++ common/com_rule.h
@@ -7,24 +7,29 @@
 * 
 * @section secSnortOption Snort规则选项
 * 
+* 以下为可以处理的选项列表
+* @li content
+* ....
+*
 * @subsection subContentOption content选项
 * 
-* content选项介绍
+* <content选项介绍>@n
+* <修饰项的含义>
 *
 * @subsection subUriOption uricontent选项
 * 
-* content选项介绍
+* <与content作同样处理>
 *
 * @subsection subPcreOption pcre选项
 * 
-* pcre选项介绍
+* <见pcre页面>@ref secPcreIntro
 *
 * @section subRuleLogic 规则逻辑结构
-* 
-* @subsection subChainStruct 选项的链式结构
 *
-* @subsection subChainMerge 选项链的合并逻辑
+* @subsection subChainStruct 选项的链式结构
+* 
+* 从数据包开头匹配的为一条链……
 *
 * @subsection subChainCompress 选项链的删除
-*
+* content包含在pcre中可删除
 */
