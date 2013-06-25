f3a71ab07b6d68bcc8497d74a8551dd9ce504f8d
haoweichen <haowch@gmail.com>
2013-06-25 10:28:33 +0800

    Add process with $

--- GroupHashTest/main.cpp
+++ GroupHashTest/main.cpp
@@ -1,153 +1,328 @@
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <tchar.h>
 #include <filesystem>
 
 #include <hwprj\ctimer.h>
-#include <hwprj\trace.h>
+#include <hwprj\unsary.h>
 #include <hwprj\compiler.h>
 #include <hwprj\compres.h>
 #include <hwprj\groupres.h>
 #include <hwprj\grouping.h>
 #include <hwprj\buildhash.h>
-#include <hwprj\logger.h>
+#include <hwprj\trace.h>
 
-bool CompareFileExt(const std::string &strFile, const std::string &strExt)
+int main()
 {
-	if (strFile.size() <= strExt.size())
-	{
-		return false;
-	}
-	if (strFile[strFile.size() - strExt.size() - 1] != '.')
-	{
-		return false;
-	}
-	typedef std::string::const_reverse_iterator STRCRITER;
-	STRCRITER rb = strFile.rbegin();
-	STRCRITER re = rb + 5;
-	for (STRCRITER ri = rb; ri != re; ++ri)
-	{
-		STRCRITER rj = strExt.rbegin() + (ri - rb);
-		if (tolower(*ri) != tolower(*rj))
-		{
-			return false;
-		}
-	}
-	return true;
-}
+	//// Defina a path object to express a directory
+	//std::tr2::sys::path rulePath("D:\\Projects\\VS2012\\SnortCompiler\\rules\\");
+	//// Construct a directory iterator for visit this path.
+	//std::tr2::sys::directory_iterator iDirCur(rulePath);
+	////the end iterator for this path.
+	//std::tr2::sys::directory_iterator iDirEnd;
 
-void main(int nArgs, char **pArgs)
-{
-	if (nArgs != 2)
-	{
-		return;
-	}
+	//CCompileResults result;
+	//char szExt[] = {'s', 'e', 'l', 'u', 'r'};
+	//for (; iDirCur != iDirEnd; ++iDirCur)
+	//{
+	//	const std::tr2::sys::path &curPath = *iDirCur;
+	//	if (!std::tr2::sys::is_directory(curPath))
+	//	{
+	//		std::string strFullName = rulePath.directory_string();
+	//		strFullName.append(curPath.directory_string());
+	//		if (strFullName.size() < 5)
+	//		{
+	//			continue;
+	//		}
+	//		std::string::reverse_iterator rb = strFullName.rbegin();
+	//		std::string::reverse_iterator re = rb + 5;
+	//		std::string::reverse_iterator ri = rb;
+	//		for (; ri != re; ++ri)
+	//		{
+	//			if (tolower(*ri) != tolower(szExt[ri - rb]))
+	//			{
+	//				break;
+	//			}
+	//		}
+	//		if (ri != re)
+	//		{
+	//			continue;
+	//		}
+	//		std::cout << strFullName << std::endl;
+	//		try
+	//		{
+	//			CompileRuleFile(strFullName.c_str(), result);
+	//		}
+	//		catch (CTrace &e)
+	//		{
+	//			std::cout << e.File() << " - " << e.Line() << ": " << e.What() << std::endl;
+	//			system("pause");
+	//		}
+	//	}
+	//}
+	//result.WriteToFile("..\\result.cdt");
 
-	std::string strPath = pArgs[1];
-	if (strPath.back() != '\\')
-	{
-		strPath.push_back('\\');
-	}
-	std::string strLogFile = strPath + "compile.log";
-	g_log.SetLogFile(strLogFile.c_str());
+	//ulong ulRuleCnt = 0;
+	//for (ulong i = 0; i < result.GetSidDfaIds().Size(); ++i)
+	//{
+	//	if (result.GetSidDfaIds()[i].m_nResult == COMPILEDINFO::RES_SUCCESS)
+	//	{
+	//		++ulRuleCnt;
+	//	}
+	//}
+	//std::cout << result.GetSidDfaIds().Size() << ": " << ulRuleCnt << std::endl;
 
-	// Define a path object to express a directory
-	std::tr2::sys::path rulePath(strPath);
-	// Construct a directory iterator for visit this path.
-	std::tr2::sys::directory_iterator iDirCur(rulePath);
-	//the end iterator for this path.
-	std::tr2::sys::directory_iterator iDirEnd;
+	//CCompileResults result;
+	//result.ReadFromFile("..\\result.cdt");
 
+	//CGroupRes groupRes;
+	//groupRes.ReadFromFile("..\\FinalResult.cdt");
+
+	//std::ofstream fout("..\\Ids.txt");
+	//for (ulong i = 0; i < groupRes.GetGroups().Size(); ++i)
+	//{
+	//	for (ulong j = 0; j < groupRes.GetGroups()[i].DfaIds.Size(); ++j)
+	//	{
+	//		fout << groupRes.GetGroups()[i].DfaIds[j] << " ";
+	//	}
+	//	fout << std::endl;
+	//}
+	//fout.clear();
+	//fout.close();
+
+	//std::ofstream foutMerge("..\\MergeId.txt");
+	//for (ulong i = 0; i < groupRes.GetGroups().Size(); ++i)
+	//{
+	//	foutMerge << groupRes.GetDfaTable()[groupRes.GetGroups()[i].mergeDfaId].GetFinalStates().CountDfaIds() << std::endl;
+	//}
+	//foutMerge.clear();
+	//foutMerge.close();
+
+	//CGroupRes groupRes;
+	//groupRes.ReadFromFile("..\\FinalResult.cdt");
+<<<<<<< develop
+
+
+	//for (ulong i = 0; i < groupRes.GetDfaTable().Size(); ++i)
+	//{
+	//	for (ulong j = 0; j < groupRes.GetDfaTable()[i].Size(); ++j)
+	//	{
+	//		int occurCnt[256] = {0};
+	//		for (ulong k = 0; k < groupRes.GetDfaTable()[i].GetGroupCount(); ++k)
+	//		{
+	//			++occurCnt[groupRes.GetDfaTable()[i][j][k]];
+	//		}
+	//	}
+	//}
+=======
+
+	//ulong nSuccess = 0;
+	//ulong nPcreError = 0;
+	//ulong nOptionError = 0;
+	//ulong nHasByte = 0;
+	//ulong nHasNot = 0;
+	//ulong nEmpty = 0;
+	//ulong nHasNoSig = 0;
+	//ulong nExceedLimit = 0;
+	//std::cout << groupRes.GetSidDfaIds().Size() << std::endl;
+	//std::ofstream fout("..\\test.txt");
+	//for (ulong i = 0; i < groupRes.GetSidDfaIds().Size(); ++i)
+	//{
+	//	if (groupRes.GetSidDfaIds()[i].m_nResult == COMPILEDINFO::RES_SUCCESS)
+	//	{
+	//		++nSuccess;
+	//	}
+	//	if (groupRes.GetSidDfaIds()[i].m_nResult & COMPILEDINFO::RES_PCREERROR)
+	//	{
+	//		++nPcreError;
+	//	}
+	//	if (groupRes.GetSidDfaIds()[i].m_nResult & COMPILEDINFO::RES_OPTIONERROR)
+	//	{
+	//		++nOptionError;
+	//		fout << groupRes.GetSidDfaIds()[i].m_nSid << std::endl;
+	//	}
+	//	if (groupRes.GetSidDfaIds()[i].m_nResult & COMPILEDINFO::RES_HASBYTE)
+	//	{
+	//		++nHasByte;
+	//	}
+	//	if (groupRes.GetSidDfaIds()[i].m_nResult & COMPILEDINFO::RES_HASNOT)
+	//	{
+	//		++nHasNot;
+	//	}
+	//	if (groupRes.GetSidDfaIds()[i].m_nResult & COMPILEDINFO::RES_EMPTY)
+	//	{
+	//		++nEmpty;
+	//	}
+	//	if (groupRes.GetSidDfaIds()[i].m_nResult & COMPILEDINFO::RES_HASNOSIG)
+	//	{
+	//		++nHasNoSig;
+	//	}
+	//	if (groupRes.GetSidDfaIds()[i].m_nResult & COMPILEDINFO::RES_EXCEEDLIMIT)
+	//	{
+	//		++nExceedLimit;
+	//	}
+	//}
+	//std::cout << nSuccess << std::endl;
+	//std::cout << nPcreError << std::endl;
+	//std::cout << nOptionError << std::endl;
+	//std::cout << nHasByte << std::endl;
+	//std::cout << nHasNot << std::endl;
+	//std::cout << nEmpty << std::endl;
+	//std::cout << nHasNoSig << std::endl;
+	//std::cout << nExceedLimit << std::endl;
+
+	//CGroupRes groupRes;
+	//groupRes.ReadFromFile("..\\FinalResult.cdt");
+
+	//ulong nReduce = 0;
+	//ulong nAll = 0;
+	//int* occurCnt = new int[256];
+	//for (ulong i = 0; i < groupRes.GetDfaTable().Size(); ++i)
+	//{
+	//	for (ulong j = 0; j < groupRes.GetDfaTable()[i].Size(); ++j)
+	//	{
+	//		std::fill(occurCnt, occurCnt + 256, 0);
+	//		for (ulong k = 0; k < groupRes.GetDfaTable()[i].GetGroupCount(); ++k)
+	//		{
+	//			ushort nVal = groupRes.GetDfaTable()[i][j][k];
+	//			if (nVal == ushort(-1))
+	//			{
+	//				++occurCnt[255];
+	//			}
+	//			else
+	//			{
+	//				++occurCnt[nVal];
+	//			}
+	//		}
+	//		int max = -1;
+	//		for (ulong k = 0; k < 256; ++k)
+	//		{
+	//			if (max < occurCnt[k])
+	//			{
+	//				max = occurCnt[k];
+	//			}
+	//		}
+	//		nReduce += groupRes.GetDfaTable()[i].GetGroupCount() - 2 * (groupRes.GetDfaTable()[i].GetGroupCount() - max + 1);
+	//		nAll += groupRes.GetDfaTable()[i].GetGroupCount();
+	//	}
+	//}
+	//delete[] occurCnt;
+	//std::cout << nReduce << std::endl;
+	//std::cout << nAll << std::endl;
+	//std::cout << nReduce / double(nAll) << std::endl;
+>>>>>>> local
+
+	CTimer ctime;
 	CCompileResults result;
-	std::string strRuleFileExt = "rules";
-	bool bCompSuc = true;
-	CTimer compTimer;
-	double dCompTime = 0;
-	std::vector<std::string> allRules;
-	for (; iDirCur != iDirEnd; ++iDirCur)
+	try
 	{
-		const std::tr2::sys::path &curPath = *iDirCur;
-		if (!std::tr2::sys::is_directory(curPath))
-		{
-			std::string strFullName = rulePath.directory_string();
-			strFullName.append(curPath.directory_string());
-			if (CompareFileExt(strFullName, strRuleFileExt))
-			{
-				//当前处理的文件
-				std::cout << strFullName << std::endl;
-				try
-				{
-					compTimer.Reset();
-					//编译规则
-					CompileRuleFile(strFullName.c_str(), result);
-					dCompTime += compTimer.Cur();
-				}
-				catch (CTrace &e)
-				{
-					std::cout << e.File() << " - " << e.Line() << ": "
-						<< e.What() << std::endl;
-					bCompSuc = false;
-					break;
-				}
-			}
-		}
+		CompileRuleFile("..\\allrules.rule", result);
 	}
-
-	if (bCompSuc == true)
+	catch (CTrace &e)
 	{
-		//编译结果文件
-		std::string strFileName(strPath);
-		strFileName += "result.cdt";
-
-		//写编译结果文件
-		result.WriteToFile(strFileName.c_str());
+		std::cout << e.File() << " - " << e.Line() << ": " << e.What() << std::endl;
+		system("pause");
 	}
+	result.WriteToFile("..\\result.cdt");
 
-	// 统计成功编译的规则数量
-	size_t nRules = 0;
-	for (size_t i = 0; i < result.GetSidDfaIds().Size(); ++i)
-	{
-		if (result.GetSidDfaIds()[i].m_nResult == COMPILEDINFO::RES_SUCCESS)
-		{
-			++nRules;
-		}
-	}
+	//result.ReadFromFile("..\\result.cdt");
 
-	compTimer.Reset();
 	CGroupRes groupRes;
-	//分组
 	Grouping(result, groupRes);
-
+	groupRes.WriteToFile("..\\GroupResult.cdt");
+	//groupRes.ReadFromFile("..\\GroupResult.cdt");
 	HASHRES HashResMap;
-	//哈希
 	HashMapping(groupRes, HashResMap);
+	groupRes.WriteToFile("..\\FinalResult.cdt");
+	//for (ulong i = 0; i < groupRes.GetDfaTable().Size(); ++i)
+	//{
+	//	groupRes.GetDfaTable()[i].MergeColumn();
+	//}
+	std::cout << "Total time: " << ctime.Reset() << std::endl;
+	std::cout << groupRes.GetGroups().Size() << std::endl;
+	std::cout << HashResMap.size() << std::endl;
 
-	//存储最终结果的文件
-	std::string strWriteFileName(strPath);
-	strWriteFileName += "FinalResult.cdt";
+	//CCompileResults result;
+	//result.ReadFromFile("..\\result.cdt");
+	//std::ifstream fin("..\\Ids.txt");
+	//std::ifstream finMerge("..\\MergeId.txt");
+	//std::vector<size_t> mergeDfaSize;
+	//size_t id;
+	//while (finMerge >> id)
+	//{
+	//	mergeDfaSize.push_back(id);
+	//}
 
-	//写最终结果文件
-	groupRes.WriteToFile(strWriteFileName.c_str());
+	//size_t count = 0;
+	//std::string str;
+	//while (std::getline(fin, str))
+	//{
+	//	//std::cout << count << std::endl;
+	//	std::stringstream ss(str);
+	//	std::vector<size_t> vecIds;
+	//	while (ss >> id)
+	//	{
+	//		vecIds.push_back(id);
+	//	}
 
-	std::ofstream fout("RunResult.log");
+	//	CDfaArray vecDfas;
+	//	vecDfas.Resize(2);
+	//	vecDfas[0] = result.GetDfaTable()[vecIds[0]];
+	//	for (ulong i = 1; i < vecIds.size(); ++i)
+	//	{
+	//		vecDfas[1] = result.GetDfaTable()[vecIds[i]];
+	//		CDfa MergeDfa;
+	//		if (!MergeMultipleDfas(vecDfas, MergeDfa))
+	//		{
+	//			std::cout << "error" << std::endl;
+	//		}
+	//		vecDfas[0] = MergeDfa;
+	//	}
+	//	if (vecDfas[0].GetFinalStates().CountDfaIds() != mergeDfaSize[count++])
+	//	{
+	//		std::cout << "error" << std::endl;
+	//	}
+	//}
 
-	fout << "Successed compiled rule: " << nRules << std::endl;
-	fout << "Total compiled chain: " << result.GetRegexTbl().Size() << std::endl;
-	fout << "Compile time: " << dCompTime << std::endl;
+	//std::ifstream fin("..\\allrules.rule");
+	//std::vector<std::string> vecRules;
+	//std::string str;
+	//while (std::getline(fin, str))
+	//{
+	//	vecRules.push_back(str);
+	//}
+	//fin.clear();
+	//fin.close();
 
-	//总分组数
-	size_t nGrounSize = groupRes.GetGroups().Size();
-	fout << "Groups: " << nGrounSize << std::endl;
-
-	fout << "Hash size: " << groupRes.GetBucketCnt() << std::endl;
-
-	//使用到的哈希槽数
-	fout << "Used hash buckets: " << HashResMap.size() << std::endl;
-
-	// 冲突率
-	fout << "Conflict rate: " <<
-		(nGrounSize - HashResMap.size()) / (float)nGrounSize << std::endl;	
-	fout << "Group and hash time: " << compTimer.Reset() << std::endl;
-
-	fout.close();
+	//CGroupRes groupRes;
+	//groupRes.ReadFromFile("..\\FinalResult.cdt");
+	//std::ofstream fout("..\\CanCompile.rule");
+	//for (ulong i = 0; i < groupRes.GetSidDfaIds().Size(); ++i)
+	//{
+	//	if (groupRes.GetSidDfaIds()[i].m_nResult == COMPILEDINFO::RES_SUCCESS)
+	//	{
+	//		str = "sid:";
+	//		std::string sid;
+	//		std::stringstream ss;
+	//		ss << groupRes.GetSidDfaIds()[i].m_nSid;
+	//		ss >> sid;
+	//		str += sid;
+	//		str += ";";
+	//		for (ulong j = 0; j < vecRules.size(); ++j)
+	//		{
+	//			if (vecRules[j].find(str, 0) != std::string::npos)
+	//			{
+	//				fout << vecRules[j] << std::endl;
+	//			}
+	//		}
+	//	}
+	//}
+	//fout.clear();
+	//fout.close();
+	//std::cout << HashResMap.size() << std::endl;
 
 	system("pause");
+	return 0;
 }
