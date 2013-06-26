ab9df60fff5a4c648a8cd5d2fea283c8fd70ee3c
devymex <devymex@gmail.com>
2013-06-26 20:54:43 +0800

    git fix a bug  in test prj

--- GroupHashTest/main.cpp
+++ GroupHashTest/main.cpp
@@ -8,43 +8,43 @@
 
 void main(int nArgs, char **pArgs)
 {
-	CTimer t;
 
 	if (nArgs != 2)
 	{
 		return;
 	}
 	
+	std::string strFile = pArgs[1];
 	//编译结果文件
-	std::string strReadFileName(pArgs[1]);
-	strReadFileName += "result.cdt";
 
 	CCompileResults result;
 	//读入编译结果文件
-	result.ReadFromFile(strReadFileName.c_str());
+	if (0 == result.ReadFromFile(strFile.c_str()))
+	{
+		CTimer t;
+		CGroupRes groupRes;
+		//分组
+		Grouping(result, groupRes);
 
-	CGroupRes groupRes;
-	//分组
-	Grouping(result, groupRes);
+		HASHRES HashResMap;
+		//哈希
+		HashMapping(groupRes, HashResMap);
 
-	HASHRES HashResMap;
-	//哈希
-	HashMapping(groupRes, HashResMap);
+		//存储最终结果的文件
+		std::string strWriteFileName(pArgs[1]);
+		strWriteFileName += "FinalResult.cdt";
 
-	//存储最终结果的文件
-	std::string strWriteFileName(pArgs[1]);
-	strWriteFileName += "FinalResult.cdt";
+		//写最终结果文件
+		groupRes.WriteToFile(strWriteFileName.c_str());
 
-	//写最终结果文件
-	groupRes.WriteToFile(strWriteFileName.c_str());
-
-	//总分组数
-	std::cout << groupRes.GetGroups().Size() << std::endl;
+		//总分组数
+		std::cout << groupRes.GetGroups().Size() << std::endl;
 	
-	//使用到的哈希槽数
-	std::cout << HashResMap.size() << std::endl;
+		//使用到的哈希槽数
+		std::cout << HashResMap.size() << std::endl;
 
-	std::cout << "Total time: " << t.Reset() << std::endl;
+		std::cout << "Total time: " << t.Reset() << std::endl;
+	}
 
 	system("pause");
 }
