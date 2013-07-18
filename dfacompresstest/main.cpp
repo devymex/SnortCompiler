8fa9832951e46f24610f8785930ed0eff42a4222
kekoukelebu <514682763@qq.com>
2013-07-18 14:36:08 +0800

    hhhh

--- dfacompresstest/main.cpp
+++ dfacompresstest/main.cpp
@@ -2,14 +2,14 @@
 #include <hwprj\groupres.h>
 #include <hwprj\dfaarray.h>
 //#include <sstream>
-//#include <hwprj\compres.h>
+#include <hwprj\compres.h>
 
 int main()
 {
 	CGroupRes groupRes;
 	groupRes.ReadFromFile("F:\\huawei\\date\\FinalResult.cdt");
 	
-	//DfaCompress(groupRes.GetDfaTable()[5]);
+	DfaCompress(groupRes.GetDfaTable()[5]);
 	system("pause");
 	return 0;
 }
