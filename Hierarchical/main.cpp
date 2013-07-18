a30faa9a2ec2bae78eaa06563e131674363870dc
bunnywj <bunny8301@gmail.com>
2013-07-18 16:23:58 +0800

    add search subgraph

--- Hierarchical/main.cpp
+++ Hierarchical/main.cpp
@@ -8,19 +8,23 @@ void main(int nArgs, char **cArgs)
 
 	for (size_t i = 0; i < CDfaSet.Size(); ++i)
 	{
-		ROWSET rows;
-		for (size_t j = 0; j < CDfaSet[i].Size(); ++j)
+		if (i == 33)
 		{
-			rows.push_back(j);
+			ROWSET rows;
+			std::cout << CDfaSet[i].Size();
+			for (size_t j = 0; j < CDfaSet[i].Size(); ++j)
+			{
+				rows.push_back(j);
+			}
+			GRAPH graph;
+
+			BuildGraph(CDfaSet[i], rows, graph);
+
+			VECROWSET vecRows;
+
+			SearchConnectSubgraph(graph, rows, vecRows);
+			HierarchicalCluster(CDfaSet[i], vecRows);
 		}
-		GRAPH graph;
-
-		BuildGraph(CDfaSet[i], rows, graph);
-
-		VECROWSET vecRows;
-
-		SearchConSubGraph(graph, vecRows);
-		HierarchicalCluster(CDfaSet[i], vecRows);
 	}
 
 	system("pause");
