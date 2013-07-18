3e3a53b9f80fecd5ccbc31d51a5732e581bd639e
serily <serilyzheng@gmail.com>
2013-07-18 10:36:15 +0800

    commit

--- dfacompresstest/main.cpp
+++ dfacompresstest/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <hwprj\groupres.h>
+#include <hwprj\dfaarray.h>
 
 int main()
 {
+	CGroupRes groupRes;
+	groupRes.ReadFromFile("F:\\cppProject\\huawei\\PreciseMatch\\CompressTest\\FinalResult.cdt");
+	std::cout << groupRes.GetDfaTable().Size() << std::endl;
+
 	system("pause");
 	return 0;
 }
