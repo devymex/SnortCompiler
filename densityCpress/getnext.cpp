05b713c0b2d27ebe1f6dbfc8d687e36e3c7def83
youwuwei <youwuwei@163.com>
2013-07-18 20:22:34 +0800

    Merge branch 'denCpress' of github.com:devymex/SnortCompiler into denCpress
    Conflicts:
    	densityCpress/getnext.cpp

--- densityCpress/getnext.cpp
+++ densityCpress/getnext.cpp
@@ -1,10 +1,33 @@
 #include "stdafx.h"
 #include "densityCpress.h"
 
-////////////
-void GetNextObj(std::vector<ushort> &orderSeeds, ROWOBJ *allObj, ROWOBJ &curobj)
+
+class seedsSort
 {
-	std::cout << std::endl;
-	std::cout << "haoweichen is so stupid!!!";
+public:
+	ROWOBJ *parrobj;
+	seedsSort (ROWOBJ *p ) : parrobj(p)
+	{}
+	
+	// 递减排序
+	bool operator () (ushort arg1, ushort arg2)
+	{
+		return parrobj[arg1].reachDis > parrobj[arg2].reachDis;
+	}
+};
+
+void GetNextObj(std::vector<ushort> &orderSeeds, ROWOBJ *arrObj, ROWOBJ &curobj)
+{		
+	
+	// 存储allObj数组的大小,此大小与orderSeeds大小等价
+	size_t sizeArr = orderSeeds.size();
+
+	// 递减排序
+	std::sort(orderSeeds.begin(), orderSeeds.end(), seedsSort(arrObj));
+
+	size_t minIdx = orderSeeds[sizeArr - 1];
+	orderSeeds.pop_back();
+
+	curobj = arrObj[minIdx];
+	
 }
-//////////
