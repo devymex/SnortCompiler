3da3f06b31f71c4a25b820b15571bc241727b530
SilentMaster <871993653@qq.com>
2013-07-18 10:59:11 +0800

    asaa

--- densityCpress/neighbor.cpp
+++ densityCpress/neighbor.cpp
@@ -1,9 +1,17 @@
 #include "stdafx.h"
 #include "densityCpress.h"
 
-void GetNeighbors(std::vector<std::vector<double>> &disMatrix, double eps, byte minPts,
-	byte *coreDis, std::vector<std::vector<byte>> &neighbors)
+void GetNeighbors(std::vector<std::vector<double>> &disMatrix, double eps, ushort minPts,
+	double *coreDis, std::vector<std::vector<ushort>> &neighbors)
 {
+	size_t i = 0, j = 0;
+	for(i = 0; i < disMatrix.size(); ++i)
+	{
+		for(j = 0; j < disMatrix[i].size(); ++j)
+		{
+			if(i != j && disMatrix[i][])
 
+		}
+	}
 }
 
