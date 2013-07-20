c76a328606a5955edab0388d519b21836eff39e5
kekoukelebu <514682763@qq.com>
2013-07-20 09:06:12 +0800

    little

--- include/hwprj/dfacompress.h
+++ include/hwprj/dfacompress.h
@@ -19,7 +19,8 @@ void UpdateMatrix(CDfa &olddfa, std::vector<CUnsignedArray> &clusterVec, ulong *
 				  std::vector<STATEID> &base, std::vector<CDfaRow> &sparseMatrix);
 
 //对dfaMatrix进行行合并压缩
-void RowMergeCompress(std::vector<CDfaRow> &dfaMatrix, std::vector<STATEID> &rowGroup, ulong &rowNum);
+void RowMergeCompress(std::vector<CDfaRow> &dfaMatrix, std::vector<STATEID> &rowGroup, 
+					  ulong &rowNum, std::vector<CDfaRow> &FinalMatrix);
 void OutPutCluster(CDfa &olddfa, std::vector<CUnsignedArray> &clusterVec, ulong *stateCluster, ulong *codeMap);//输出测试
 
 void OutputMatrix(std::vector<STATEID> &base, std::vector<CDfaRow> &dfaMatrix, std::vector<CDfaRow> &sparseMatrix);//输出提取base后的矩阵以及稀疏矩阵
