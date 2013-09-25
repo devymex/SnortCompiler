30eac59931db12981f9ebfcfc19179feaaaf8bfe
devymex <devymex@gmail.com>
2013-09-25 17:09:58 +0800

    match packet

---
+++ MatchPacket/MatchPkt.h
@@ -0,0 +1,172 @@
+#pragma once
+
+
+#include <Windows.h>
+#include <pcap.h>
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+
+#include <hwprj\dfa.h>
+#include <hwprj\ruleoption.h>
+#include <hwprj\regrule.h>
+#include <hwprj\compiler.h>
+#include <hwprj\pcrechain.h>
+#include <hwprj\snortrule.h>
+#include <hwprj\pcrematch.h>
+#include <hwprj\buildhash.h>
+
+
+#define ETHDRLEN 14 
+#define _TCP 6
+#define _UDP 17
+#define UDPHDRLEN 8
+
+#ifndef MATCHPKT_H_
+#define MATCHPKT __declspec(dllimport)
+#else
+#define MATCHPKT __declspec(dllexport)
+#endif
+
+extern ulong g_pktnum;
+
+typedef struct ip_address{
+    u_char byte1;
+    u_char byte2;
+    u_char byte3;
+    u_char byte4;
+}ip_address;
+
+/*! 4字节的IP地址 */
+typedef struct ip_header{
+    u_char  ver_ihl;        // 版本 (4 bits) + 首部长度 (4 bits)
+    u_char  tos;            // 服务类型(Type of service) 
+    u_short tlen;           // 总长(Total length)  
+    u_short identification; // 标识(Identification)
+    u_short flags_fo;       // 标志位(Flags) (3 bits) + 段偏移量(Fragment offset) (13 bits)
+    u_char  ttl;            // 存活时间(Time to live)
+    u_char  proto;          // 协议(Protocol)
+    u_short crc;            // 首部校验和(Header checksum)
+    ip_address  saddr;      // 源地址(Source address)
+    ip_address  daddr;      // 目的地址(Destination address)
+    u_int   op_pad;         // 选项与填充(Option + Padding)
+}ip_header;
+
+/* UDP 首部*/
+typedef struct udp_header{
+    u_short sport;          // 源端口(Source port)
+    u_short dport;          // 目的端口(Destination port)
+    u_short len;            // UDP数据包长度(Datagram length)
+    u_short crc;            // 校验和(Checksum)
+}udp_header;
+
+typedef struct tcp_header //定义TCP首部
+
+{
+
+u_short saddr;           //16位源端口
+
+u_short daddr;           //16位目的端口
+
+u_int seq;               //32位序列号
+
+u_int ack;               //32位确认号
+
+u_char lenres;           //4位首部长度/6位保留字
+
+u_char flag;             //6位标志位
+
+u_short window;          //16位窗口大小
+
+u_short crc;             //16位校验和
+
+u_short urp;             //16位紧急数据偏移量
+
+}tcp_header;
+
+inline ulong HashFcn(SIGNATURE sig)
+{
+	u_char *cSig = (u_char *)&sig;
+
+	const ulong _FNV_offset_basis = 2166136261U;
+	const ulong _FNV_prime = 16777619U;
+
+	ulong _Val = _FNV_offset_basis;	
+	for(ulong i = 0; i < 4; ++i)
+	{
+		_Val ^= cSig[i];
+		_Val *= _FNV_prime;
+	}
+
+	return (_Val);
+}
+typedef void (__stdcall*PACKETRECV) (const ip_header *ih, const byte *data, void* pParam);
+
+struct PACKETPARAM
+{
+	void *pUser;
+	PACKETRECV pFunc;
+};
+
+struct MATCHRESULT
+{
+	ulong g_pktnum;
+	std::vector<ulong> matchedSids;
+};
+
+struct SIGSID
+{
+	ulong sid;
+	SIGNATURE sig;
+};
+
+
+struct REGRULES
+{
+	ulong m_nSid;
+	CRegRule regrule;
+};
+
+struct SIG_HASH
+{
+	ulong operator()(const SIGNATURE &str)
+	{
+		const ulong _FNV_offset_basis = 2166136261U;
+		const ulong _FNV_prime = 16777619U;
+
+		ulong _Val = _FNV_offset_basis;	
+		for(ulong i = 0; i < 4; ++i)
+		{
+			_Val ^= str >> (8 * i);
+			_Val *= _FNV_prime;
+		}
+
+		return (_Val);
+	}
+};
+
+typedef std::unordered_map<SIGNATURE, std::vector<ulong>, SIG_HASH> SIGSMAP;
+struct REGRULESMAP
+{
+	std::vector<REGRULES> result;
+	SIGSMAP sigmap;
+	std::ofstream mchresult;
+	std::string resultpath;
+};
+
+void __stdcall PktParam(const ip_header *ih, const byte *data, void* user);
+
+void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data);
+bool MyLoadCapFile(const char* pFile, PACKETRECV cv, void* pUser);
+MATCHPKT bool LoadCapFile(const char* pFile, void* pUser);
+
+void __stdcall MyProcess(const CSnortRule &rule, LPVOID lpParam);
+MATCHPKT void MchCompile(const char* filename, LPVOID result);
+MATCHPKT bool TradithinalMatch(std::vector<u_char> &dataSrc, CRegRule &regRule);
+
+void GetMchRule(const u_char *data, ulong len, void* user, std::vector<ulong> &rules);
+void HdlOnePkt(const u_char *data, ulong len, void*user);
+bool MyPcreMatch(const u_char *data, ulong len, CRegRule &regRule);
+MATCHPKT void HandleAllFile(const std::string &path, void* user);
---
+++ MatchPacket/main.cpp
@@ -0,0 +1,110 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include "DfaMatch.h"
+
+//
+//struct GROUPNUM
+//{
+//	CSIGNATURES ComSigs;
+//	std::size_t mergeDfaId;
+//};
+
+void main(int nArgc, char **pArgs)
+{
+	PCREDFA pcredfa;
+
+	DFAMCH &dfamch = pcredfa.dfamch;
+	//dfamch.resultFolder = "..\\..\\output\\dmatchresult"; //一个数据包文件对应一个txt结果
+	CGroupRes &groupRes = dfamch.mergedDfas;
+	HASHRES &hashResMap = dfamch.hashtable;
+	groupRes.ReadFromFile(pArgs[1]); //输入的编译DFA
+	hash.nBucketCnt = groupRes.GetBucketCnt();
+	for(size_t i = 0; i < groupRes.GetGroups().Size(); ++i)
+	{
+		HASHNODE hashnode;
+		hashnode.m_nDfaId = groupRes.GetGroups()[i].mergeDfaId;
+		hashnode.m_sig = groupRes.GetGroups()[i].currSig;
+		hashResMap[hash(groupRes.GetGroups()[i].currSig)].push_back(hashnode);
+	}
+
+	DfaidSidMap(groupRes, dfamch.dIdSId);
+	REGRULESMAP &rulesmap = pcredfa.rulesmap;
+	//MchCompile("..\\..\\input\\CanCompile.rule", &rulesmap);
+	//rulesmap.resultpath = "..\\..\\output\\pmatchresult";
+
+	std::string path = pArgs[2]; //输入的数据包路径 *.cap
+	PDHandleAllFile(path, &pcredfa);
+
+	std::cout << "Total packets: " << g_ulAllDp << std::endl;
+	std::cout << "Hit packets: " << g_ulHashed << std::endl;
+
+	system("pause");
+}
+
+
+
+//#include "stdafx.h"
+//#include "MatchPkt.h"
+//
+//#include <fstream>
+//#include <tchar.h>
+//
+//void main()
+//{
+//	REGRULESMAP rulesmap;
+//	MchCompile("..\\..\\input\\CanCompile.rule", &rulesmap);
+//
+//	rulesmap.resultpath = "..\\..\\output\\pmatchresult";
+//	HandleAllFile("E:\\allPkt", &rulesmap);
+//}
+
+//#include "stdafx.h"
+//#include "DfaMatch.h"
+//
+//void main()
+//{
+//	std::string dpath1 = "D:\\projects\\output\\backup\\d",
+//		dpath2 = "D:\\projects\\output\\dmatchresult";
+//
+//	std::vector<std::string> result1, result2;
+//	result1.reserve(400);
+//	result2.reserve(400);
+//
+//	ResultFiles(dpath1, result1);
+//	ResultFiles(dpath2, result2);
+//
+//	size_t max = result1.size();
+//	if (result2.size() > max)
+//	{
+//		max = result2.size();
+//	}
+//
+//	std::ofstream ofs;
+//	ofs.open("D:\\projects\\output\\compare.txt");
+//
+//	for(size_t i = 0; i < max; ++i)
+//	{
+//		std::ifstream ifs1(result1[i]);
+//		std::ifstream ifs2(result2[i]);
+//
+//		std::string tem1, tem2, firline1, firline2;
+//		std::getline(ifs1, firline1);
+//		std::getline(ifs2, firline2);
+//
+//		ofs << firline1 << std::endl;
+//
+//		while (std::getline(ifs1, tem1), std::getline(ifs2, tem2))
+//		{
+//			if( tem1.compare(tem2) != 0)
+//			{
+//				ofs << "different:  " << tem1 << std::endl << tem2 << std::endl;
+//			}
+//		}
+//
+//
+//	}
+//	std::cout << std::endl;
+//}
